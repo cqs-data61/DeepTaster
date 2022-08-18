d8c91472816e76503f29cafc292997c80ac12fa3
qkrtjsgp08 <qkrtjsgp08@naver.com>
2022-08-18 16:52:05 +0900

    Update detection_classifier_generation.py

--- detection_classifier_generation.py
+++ detection_classifier_generation.py
@@ -19,6 +19,8 @@ from deepsvdd import DeepSVDD_network, pretrain_autoencoder, TrainerDeepSVDD
 parser = argparse.ArgumentParser(description='DFT image generation')
 parser.add_argument('--train', required=True, type=str, help='train dataset path')
 parser.add_argument('--val', default='cifar100', type=str, help='validation dataset path')
+parser.add_argument('--preepochs', default=40, type=int, help='autoencoder training epochs')
+parser.add_argument('--epochs', default=5, type=int, help='detection classifier training epochs')
 parser.add_argument('--saveautoencoder', default='./', type=str, help='autoencoder saved dir')
 parser.add_argument('--output', default='./', type=str, help='detection classifier saved dir')
 
@@ -54,8 +56,8 @@ if __name__ == '__main__':
 
   device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
   args = easydict.EasyDict({
-         'num_epochs':10,
-         'num_epochs_ae':40,
+         'num_epochs':opt.epochs,
+         'num_epochs_ae':opt.preepochs,
          'lr':1e-3,
          'lr_ae':1e-3,
          'weight_decay':5e-7,
@@ -85,7 +87,7 @@ if __name__ == '__main__':
               milestones=args.lr_milestones, gamma=0.1)
 
   net.train()
-  for epoch in range(2):
+  for epoch in range(opt.epochs):
       total_loss = 0
       for x, _ in train_dataloader:
           x = x.float().to(device)
@@ -101,369 +103,14 @@ if __name__ == '__main__':
       print('Training Deep SVDD... Epoch: {}, Loss: {:.3f}'.format(
               epoch+1, total_loss/len(train_dataloader)))
 
-  labels,scores=eval(net,c,test_dataloader,device)
-
-  import matplotlib.pyplot as plt
-  unless=[0]*10
-  for i in range(len(labels)):
-    if labels[i]==0:
-      if unless[0]==1: 
-        plt.plot(i,scores[i],'.', color='black')
-      else:
-        plt.plot(i,scores[i],'.', color='black',label="cifar_densenet")
-        unless[0]=1
-    elif labels[i]==1:
-      if unless[1]==1: 
-        plt.plot(i,scores[i],'.', color='r')
-      else:
-        plt.plot(i,scores[i],'.', color='r',label="cifar_resnet")
-        unless[1]=1
-    elif labels[i]==2:
-      if unless[2]==1: 
-        plt.plot(i,scores[i],'.', color='g')
-      else:
-        plt.plot(i,scores[i],'.', color='g',label="cifar_vgg")
-        unless[2]=1
-    elif labels[i]==3:
-      if unless[3]==1: 
-        plt.plot(i,scores[i],'.', color='blue')
-      else:
-        plt.plot(i,scores[i],'.', color='blue',label="imagenet_densenet")
-        unless[3]=1
-    elif labels[i]==4:
-      if unless[4]==1: 
-        plt.plot(i,scores[i],'.', color='blue')
-      else:
-        plt.plot(i,scores[i],'.', color='blue',label="imagenet_resnet")
-        unless[4]=1
-    elif labels[i]==5:
-      if unless[5]==1: 
-        plt.plot(i,scores[i],'.', color='blue')
-      else:
-        plt.plot(i,scores[i],'.', color='blue',label="imagenet_vgg")
-        unless[5]=1
-    elif labels[i]==6:
-      if unless[6]==1: 
-        plt.plot(i,scores[i],'.', color='orange')
-      else:
-        plt.plot(i,scores[i],'.', color='orange',label="val_densenet")
-        unless[6]=1
-    elif labels[i]==7:
-      if unless[7]==1: 
-        plt.plot(i,scores[i],'.', color='orange')
-      else:
-        plt.plot(i,scores[i],'.', color='orange',label="val_resnet")
-        unless[7]=1
-    elif labels[i]==8:
-      if unless[8]==1: 
-        plt.plot(i,scores[i],'.', color='orange')
-      else:
-        plt.plot(i,scores[i],'.', color='orange',label="val_vgg")
-        unless[8]=1
-  plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
-  plt.tight_layout()
-
-  import numpy
-
   scores = []
   net.eval()
   print('Testing...')
   with torch.no_grad():
-      for x, y in test_dataloader:
+      for x, y in val_dataloader:
           x = x.float().to(device)
           z = net(x)
           score = torch.sum((z - c) ** 2, dim=1)
           for i in range(16):
-            if y[i]==6 or y[i]==7 or y[i]==8:
-              scores.append(score[i])
-  print(numpy.mean(scores),numpy.std(scores))
-
-  import math
-  scores.sort()
-  threshold=scores[855]
-  print(threshold)
-
-  scores = []
-  net.eval()
-  print('Testing...')
-  image_max=0
-  with torch.no_grad():
-      for x, y in test_dataloader:
-          x = x.float().to(device)
-          z = net(x)
-          score = torch.sum((z - c) ** 2, dim=1)
-          for i in range(16):
-            if y[i]==0 or y[i]==1 or y[i]==2:
-              if score[i]<=threshold:
-                image_max+=1
-
-  print(image_max,round(image_max/864,4))
-
-  scores = []
-  net.eval()
-  print('Testing...')
-  image_max=0
-  with torch.no_grad():
-      for x, y in test_dataloader:
-          x = x.float().to(device)
-          z = net(x)
-          score = torch.sum((z - c) ** 2, dim=1)
-          for i in range(16):
-            if y[i]==3 or y[i]==4 or y[i]==5:
-              if score[i]>threshold:
-                image_max+=1
-
-  print(image_max,round(image_max/864,4))
-
-
-
-
-
-
-
-
-
-
-
-  torch.save(net,  '/content/drive/MyDrive/Watermark_dnn/deep_one_class/Imagenet5.pt')
-
-
-
-  !rm -r /content/drive/MyDrive/Watermark_dnn/cifar_dataset_experiment/office_scratch/images2/resnet_200
-
-
-
-  #test 
-  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-  state_dict = torch.load('/content/drive/MyDrive/Watermark_dnn/deep_one_class/cifar_1600_1_parameter.pth')
-  c1 = torch.Tensor(state_dict['center']).to(device)
-  net1= torch.load('/content/drive/MyDrive/Watermark_dnn/deep_one_class/cifar_1600_1.pt')
-
-  import torchvision.datasets as dset
-  dataset = dset.ImageFolder(root="/content/drive/MyDrive/Watermark_dnn/cifar_dataset_experiment/cifar_test_224",
-                             transform=transforms.Compose([
-                                 transforms.Grayscale(),
-                                 transforms.Resize(28),      
-                                 transforms.CenterCrop(28), 
-                                 transforms.ToTensor(),    
-                             ]))
-
-  dataloader = torch.utils.data.DataLoader(dataset,
-                                           batch_size=16,
-                                           shuffle=False,
-                                           num_workers=8)
-
-  #.평균 표편 구하기
-  import numpy
-
-  scores = []
-  net1.eval()
-  print('Testing...')
-  with torch.no_grad():
-      for x, y in dataloader:
-          for i in range(16):
-            if y[i]==8 or y[i]==9 or y[i]==10:      
-              x = x.float().to(device)
-              z = net1(x)
-              score = torch.sum((z - c1) ** 2, dim=1)
-              scores.append(score[i])
-  print(numpy.mean(scores),numpy.std(scores))
-
-  #threshold 구하기
-  scores.sort()
-  print(scores[855]*1000)
-  print(len(scores))
-
-
-
-  #threshold 구하기
-  import math
-  threshold=scores[855]
-  #threshold=scores[570]
-
-  print(threshold)
-
-
-
-  import torchvision.datasets as dset
-  dataset = dset.ImageFolder(root="/content/drive/MyDrive/Watermark_dnn/cifar_dataset_experiment/transfer_sp/resnet_9/images",
-                             transform=transforms.Compose([
-                                 transforms.Grayscale(),
-                                 transforms.Resize(28),      
-                                 transforms.CenterCrop(28), 
-                                 transforms.ToTensor(),    
-                             ]))
-
-  dataloader = torch.utils.data.DataLoader(dataset,
-                                           batch_size=16,
-                                           shuffle=False,
-                                           num_workers=8)
-
-  #accuracy 구하기
-  for k in range(3):
-    scores = []
-    net1.eval()
-    print('Testing...')
-    image_max=0
-    with torch.no_grad():
-        for x, y in dataloader:          
-            for i in range(16):
-              if y[i]==k:
-                x = x.float().to(device)
-                z = net1(x)
-                score = torch.sum((z - c1) ** 2, dim=1)
-                if score[i]<=threshold:
-                  image_max+=1
-
-    print(k, ": ",image_max,round(image_max/288,4))
-
-  #최소최대 구하기
-  scores = []
-  net1.eval()
-  print('Testing...')
-  image_max=0
-  image_min=1
-  with torch.no_grad():
-      for x, y in dataloader:
-          x = x.float().to(device)
-          z = net1(x)
-          score = torch.sum((z - c1) ** 2, dim=1)
-          for i in range(16):
-            if y[i]==8:
-              if score[i]>image_max:
-                image_max=score[i]
-              if score[i]<image_min:
-                image_min=score[i]
-          scores.append(score.detach().cpu())
-  print(image_max,image_min)
-
-  labels,scores=eval(net1,c1,dataloader,device)
-
-  #imagenet
-  import matplotlib.pyplot as plt
-  unless=[0]*9
-  for i in range(len(labels)):
-    if labels[i]==0:
-      if unless[0]==1: 
-        plt.plot(i,scores[i],'.', color='black')
-      else:
-        plt.plot(i,scores[i],'.', color='black',label="Imagenet_resnet")
-        unless[0]=1
-    elif labels[i]==1:
-      if unless[1]==1: 
-        plt.plot(i,scores[i],'.', color='r')
-      else:
-        plt.plot(i,scores[i],'.', color='r',label="Imagenet_dense")
-        unless[1]=1
-    elif labels[i]==2:
-      if unless[2]==1: 
-        plt.plot(i,scores[i],'.', color='g')
-      else:
-        plt.plot(i,scores[i],'.', color='g',label="Imagenet_google")
-        unless[2]=1
-    elif labels[i]==3:
-      if unless[3]==1: 
-        plt.plot(i,scores[i],'.', color='y')
-      else:
-        plt.plot(i,scores[i],'.', color='y',label="Imagenet_vgg")
-        unless[3]=1
-    elif labels[i]==4:
-      if unless[4]==1: 
-        plt.plot(i,scores[i],'.', color='blue')
-      else:
-        plt.plot(i,scores[i],'.', color='blue',label="resnet_car")
-        unless[4]=1
-    elif labels[i]==5:
-      if unless[5]==1: 
-        plt.plot(i,scores[i],'.', color='orange')
-      else:
-        plt.plot(i,scores[i],'.', color='orange',label="resnet_CIFAR_25")
-        unless[5]=1
-    elif labels[i]==6:
-      if unless[6]==1: 
-        plt.plot(i,scores[i],'.', color='purple')
-      else:
-        plt.plot(i,scores[i],'.', color='purple',label="resnet_CIFAR_50")
-        unless[6]=1
-    elif labels[i]==7:
-      if unless[7]==1: 
-        plt.plot(i,scores[i],'.', color='grey')
-      else:
-        plt.plot(i,scores[i],'.', color='grey',label="resnet_CIFAR_75")
-        unless[7]=1
-    elif labels[i]==8:
-      if unless[8]==1: 
-        plt.plot(i,scores[i],'.', color='indigo')
-      else:
-        plt.plot(i,scores[i],'.', color='indigo',label="resnet_cifar")
-        unless[8]=1
-  plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
-  plt.tight_layout()
-
-  labels,scores=eval(net,c,test_dataloader,device)
-
-  #cifar
-  import matplotlib.pyplot as plt
-  unless=[0]*9
-  for i in range(len(labels)):
-    if labels[i]==0:
-      if unless[0]==1: 
-        plt.plot(i,scores[i],'.', color='r')
-      else:
-        plt.plot(i,scores[i],'.', color='r',label="cifar_inception")
-        unless[0]=1
-    elif labels[i]==1:
-      if unless[1]==1: 
-        plt.plot(i,scores[i],'.', color='r')
-      else:
-        plt.plot(i,scores[i],'.', color='r',label="cifar_resnet")
-        unless[1]=1
-    elif labels[i]==2:
-      if unless[2]==1: 
-        plt.plot(i,scores[i],'.', color='b')
-      else:
-        plt.plot(i,scores[i],'.', color='b',label="cifar_vgg")
-        unless[2]=1
-    elif labels[i]==3:
-      if unless[3]==1: 
-        plt.plot(i,scores[i],'.', color='r')
-      else:
-        plt.plot(i,scores[i],'.', color='r',label="10_10")
-        unless[3]=1
-    # elif labels[i]==4:
-    #   if unless[4]==1: 
-    #     plt.plot(i,scores[i],'.', color='blue')
-    #   else:
-    #     plt.plot(i,scores[i],'.', color='blue',label="resnet_car")
-    #     unless[4]=1
-    # elif labels[i]==5:
-    #   if unless[5]==1: 
-    #     plt.plot(i,scores[i],'.', color='b')
-    #   else:
-    #     plt.plot(i,scores[i],'.', color='b',label="resnet_CIFAR_25")
-    #     unless[5]=1
-    # elif labels[i]==6:
-    #   if unless[6]==1: 
-    #     plt.plot(i,scores[i],'.', color='y')
-    #   else:
-    #     plt.plot(i,scores[i],'.', color='y',label="resnet_CIFAR_50")
-    #     unless[6]=1
-    # elif labels[i]==7:
-    #   if unless[7]==1: 
-    #     plt.plot(i,scores[i],'.', color='black')
-    #   else:
-    #     plt.plot(i,scores[i],'.', color='black',label="resnet_CIFAR_75")
-    #     unless[7]=1
-    # elif labels[i]==8:
-    #   if unless[8]==1: 
-    #     plt.plot(i,scores[i],'.', color='black')
-    #   else:
-    #     plt.plot(i,scores[i],'.', color='black',label="resnet_cifar")
-    #     unless[8]=1
-  plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
-  plt.tight_layout()
-
-
-
-  print(np.std([98.61,99.65,95.49]))
-
+            scores.append(score[i])
+  print(np.mean(scores),np.std(scores))
