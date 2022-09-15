c74647afe75300fb79b822be2eeb07eab7a724c1
qkrtjsgp08 <qkrtjsgp08@naver.com>
2022-09-15 14:10:48 +0900

    Update fine_tuning.py

--- attack/fine_tuning.py
+++ attack/fine_tuning.py
@@ -59,13 +59,8 @@ def train_model(vgg, criterion, optimizer, scheduler, num_epochs=10):
             if i % 100 == 0:
                 print("\rTraining batch {}/{}".format(i, train_batches / 2), end='', flush=True)
                 
-            # Use half training dataset
-            if i >= train_batches / 2:
-                break
-                
             inputs, labels = data
-            
-
+          
             inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
 
             optimizer.zero_grad()
