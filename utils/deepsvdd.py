c9d6c9e35932f0298655426e86a5650b41b4d18e
Seonhye Park <qkrtjsgp08@naver.com>
2023-08-29 08:56:01 +0900

    Add files via upload

---
+++ utils/deepsvdd.py
@@ -0,0 +1,200 @@
+import numpy as np
+import easydict 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils import data
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from PIL import Image
+from sklearn.metrics import roc_auc_score
+
+
+
+class DeepSVDD_network(nn.Module):
+    def __init__(self, z_dim=32):
+        super(DeepSVDD_network, self).__init__()
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.conv1 = nn.Conv2d(1, 8, 5, bias=False, padding=2)
+        self.bn1 = nn.BatchNorm2d(8, eps=1e-04, affine=False)
+        self.conv2 = nn.Conv2d(8, 4, 5, bias=False, padding=2)
+        self.bn2 = nn.BatchNorm2d(4, eps=1e-04, affine=False)
+        self.fc1 = nn.Linear(4 * 7 * 7, z_dim, bias=False)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool(F.leaky_relu(self.bn1(x)))
+        x = self.conv2(x)
+        x = self.pool(F.leaky_relu(self.bn2(x)))
+        x = x.view(x.size(0), -1)
+        return self.fc1(x)
+
+
+class pretrain_autoencoder(nn.Module):
+    def __init__(self, z_dim=32):
+        torch.manual_seed(21)
+        super(pretrain_autoencoder, self).__init__()
+        self.z_dim = z_dim
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.conv1 = nn.Conv2d(1, 8, 5, bias=False, padding=2)
+        self.bn1 = nn.BatchNorm2d(8, eps=1e-04, affine=False)
+        self.conv2 = nn.Conv2d(8, 4, 5, bias=False, padding=2)
+        self.bn2 = nn.BatchNorm2d(4, eps=1e-04, affine=False)
+        self.fc1 = nn.Linear(4 * 7 * 7, z_dim, bias=False)
+
+        self.deconv1 = nn.ConvTranspose2d(2, 4, 5, bias=False, padding=2)
+        self.bn3 = nn.BatchNorm2d(4, eps=1e-04, affine=False)
+        self.deconv2 = nn.ConvTranspose2d(4, 8, 5, bias=False, padding=3)
+        self.bn4 = nn.BatchNorm2d(8, eps=1e-04, affine=False)
+        self.deconv3 = nn.ConvTranspose2d(8, 1, 5, bias=False, padding=2)
+        
+    def encoder(self, x):
+        x = self.conv1(x)
+        x = self.pool(F.leaky_relu(self.bn1(x)))
+        x = self.conv2(x)
+        x = self.pool(F.leaky_relu(self.bn2(x)))
+        x = x.view(x.size(0), -1)
+        return self.fc1(x)
+   
+    def decoder(self, x):
+        x = x.view(x.size(0), int(self.z_dim / 16), 4, 4)
+        x = F.interpolate(F.leaky_relu(x), scale_factor=2)
+        x = self.deconv1(x)
+        x = F.interpolate(F.leaky_relu(self.bn3(x)), scale_factor=2)
+        x = self.deconv2(x)
+        x = F.interpolate(F.leaky_relu(self.bn4(x)), scale_factor=2)
+        x = self.deconv3(x)
+        return torch.sigmoid(x)
+        
+
+    def forward(self, x):
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        return x_hat
+
+class TrainerDeepSVDD:
+    def __init__(self, args, data_loader, device, path):
+        self.args = args
+        self.train_loader = data_loader
+        self.device = device
+        self.path = path
+
+    def pretrain(self):
+        """ Training autoencoder"""
+        torch.manual_seed(21)
+        ae = pretrain_autoencoder(self.args.latent_dim).to(self.device)
+        ae.apply(weights_init_normal)
+        optimizer = torch.optim.Adam(ae.parameters(), lr=self.args.lr_ae,
+                               weight_decay=self.args.weight_decay_ae)
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, 
+                    milestones=self.args.lr_milestones, gamma=0.1)
+        
+        ae.train()
+        for epoch in range(self.args.num_epochs_ae):
+            total_loss = 0
+            for x, _ in self.train_loader:
+                x = x.float().to(self.device)
+                
+                optimizer.zero_grad()
+                x_hat = ae(x)
+                reconst_loss = torch.mean(torch.sum((x_hat - x) ** 2, dim=tuple(range(1, x_hat.dim()))))
+                reconst_loss.backward()
+                optimizer.step()
+                
+                total_loss += reconst_loss.item()
+            scheduler.step()
+            print('Pretraining Autoencoder... Epoch: {}, Loss: {:.3f}'.format(
+                   epoch, total_loss/len(self.train_loader)))
+        self.save_weights_for_DeepSVDD(ae, self.train_loader, self.path) 
+    
+
+    def save_weights_for_DeepSVDD(self, model, dataloader, path):
+        c = self.set_c(model, dataloader)
+        net = DeepSVDD_network(self.args.latent_dim).to(self.device)
+        state_dict = model.state_dict()
+        net.load_state_dict(state_dict, strict=False)
+        torch.save({'center': c.cpu().data.numpy().tolist(),
+                    'net_dict': net.state_dict()}, path)
+    
+
+    def set_c(self, model, dataloader, eps=0.1):
+        """Initializing the center for the hypersphere"""
+        model.eval()
+        z_ = []
+        with torch.no_grad():
+            for x, _ in dataloader:
+                x = x.float().to(self.device)
+                z = model.encoder(x)
+                z_.append(z.detach())
+        z_ = torch.cat(z_)
+        c = torch.mean(z_, dim=0)
+        c[(abs(c) < eps) & (c < 0)] = -eps
+        c[(abs(c) < eps) & (c > 0)] = eps
+        return c
+
+    def train(self, path):
+        """Train Deep SVDD model"""
+        torch.manual_seed(21)
+        net = DeepSVDD_network().to(self.device)
+        
+        if self.args.pretrain==True:
+            state_dict = torch.load(path)
+            net.load_state_dict(state_dict['net_dict'])
+            c = torch.Tensor(state_dict['center']).to(self.device)
+        else:
+            net.apply(weights_init_normal)
+            c = torch.randn(self.args.latent_dim).to(self.device)
+        
+        optimizer = torch.optim.Adam(net.parameters(), lr=self.args.lr,
+                               weight_decay=self.args.weight_decay)
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, 
+                    milestones=self.args.lr_milestones, gamma=0.1)
+
+        net.train()
+        for epoch in range(self.args.num_epochs):
+            total_loss = 0
+            for x, _ in self.train_loader:
+                x = x.float().to(self.device)
+
+                optimizer.zero_grad()
+                z = net(x)
+                loss = torch.mean(torch.sum((z - c) ** 2, dim=1))
+                loss.backward()
+                optimizer.step()
+
+                total_loss += loss.item()
+            scheduler.step()
+            print('Training Deep SVDD... Epoch: {}, Loss: {:.3f}'.format(
+                   epoch, total_loss/len(self.train_loader)))
+        self.net = net
+        self.c = c
+
+        return self.net, self.c
+        
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1 and classname != 'Conv':
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("Linear") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+def eval(net, c, dataloader, device):
+    """Testing the Deep SVDD model"""
+
+    scores = []
+    labels = []
+    net.eval()
+    print('Testing...')
+    with torch.no_grad():
+        for x, y in dataloader:
+            x = x.float().to(device)
+            z = net(x)
+            score = torch.sum((z - c) ** 2, dim=1)
+
+            scores.append(score.detach().cpu())
+            labels.append(y.cpu())
+    labels, scores = torch.cat(labels).numpy(), torch.cat(scores).numpy()
+    # print('ROC AUC score: {:.2f}'.format(roc_auc_score(labels, scores)*100))
+    return labels, scores
